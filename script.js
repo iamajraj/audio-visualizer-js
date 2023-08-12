dcee6c02fdd8433a44892220216ccdfd212d5994
iamajraj <akmalraj07@gmail.com>
2023-08-12 10:15:21 +0600

    update: user can now select the audio, some styles are added

--- script.js
+++ script.js
@@ -1,45 +1,137 @@
-const canvas = document.getElementById('visualizer');
+// let audioContext;
+// let analyser;
+// const volumeCtr = document.querySelector('.volume-ctr');
+// const dataArray = new Uint8Array(256);
+
+// let selectedAudio = '';
+
+// async function initializeAudio(audioSrc) {
+//   audioContext = new (window.AudioContext || window.webkitAudioContext)();
+//   analyser = audioContext.createAnalyser();
+//   analyser.fftSize = 256;
+//   const audioElement = new Audio(audioSrc);
+//   const audioSource = audioContext.createMediaElementSource(audioElement);
+//   audioSource.connect(analyser);
+//   analyser.connect(audioContext.destination);
+
+//   await audioElement.play();
+//   audioElement.volume = volumeCtr.value / 100;
+
+//   const canvas = document.getElementById('visualizer');
+//   const ctx = canvas.getContext('2d');
+
+//   volumeCtr.addEventListener('input', (ev) => {
+//     audioElement.volume = ev.target.value / 100;
+//   });
+
+//   function draw() {
+//     analyser.getByteFrequencyData(dataArray);
+//     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+//     canvas.width = canvas.parentElement.clientWidth; // Set canvas width to match its container
+//     canvas.height = 150; // Set canvas height
+
+//     const barWidth = canvas.width / dataArray.length;
+
+//     for (let i = 0; i < dataArray.length; i++) {
+//       const barHeight = dataArray[i];
+//       const r = barHeight + 25 * (i / dataArray.length);
+//       const g = 250 * (i / dataArray.length);
+//       const b = 50;
+//       ctx.fillStyle = `rgb(${r},${g},${b})`;
+//       ctx.fillRect(
+//         i * barWidth,
+//         canvas.height - barHeight,
+//         barWidth,
+//         barHeight
+//       );
+//     }
+
+//     requestAnimationFrame(draw);
+//   }
+
+//   draw();
+// }
+
+// const audioInput = document.getElementById('audioInput');
+// audioInput.addEventListener('change', (event) => {
+//   const selectedFile = event.target.files[0];
+//   if (selectedFile) {
+//     selectedAudio = selectedFile;
+//     updateSelectedAudioName();
+//     playButton.disabled = false;
+//   }
+// });
+
+// const playButton = document.getElementById('playButton');
+// playButton.addEventListener('click', async () => {
+//   if (selectedAudio) {
+//     const audioSrc = URL.createObjectURL(selectedAudio);
+//     await initializeAudio(audioSrc);
+//   }
+//   playButton.disabled = true;
+// });
+
+// const playDefaultButton = document.getElementById('playDefaultButton');
+// playDefaultButton.addEventListener('click', () => {
+//   initializeAudio('tala-al.mp3');
+//   playButton.disabled = true;
+// });
+
+// function updateSelectedAudioName() {
+//   const selectedAudioNameElement = document.getElementById(
+//     'selected-audio-name'
+//   );
+//   selectedAudioNameElement.textContent = selectedAudio.name;
+// }
+
+let audioContext;
+let analyser;
 const volumeCtr = document.querySelector('.volume-ctr');
+const dataArray = new Uint8Array(256);
+let audioElement;
 
-const ctx = canvas.getContext('2d');
+let selectedAudio = '';
 
-const audioElement = new Audio('tala-al.mp3');
+async function initializeAudio(audioSrc) {
+  if (audioContext) {
+    audioContext.close();
+  }
 
-const initializeAudio = async () => {
-  // Create an analyser node
-  const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-  const analyser = audioContext.createAnalyser();
+  audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  analyser = audioContext.createAnalyser();
   analyser.fftSize = 256;
-  const bufferLength = analyser.frequencyBinCount;
-  const dataArray = new Uint8Array(bufferLength);
+  audioElement = new Audio(audioSrc);
+  const audioSource = audioContext.createMediaElementSource(audioElement);
+  audioSource.connect(analyser);
+  analyser.connect(audioContext.destination);
+
+  await audioElement.play();
+  audioElement.volume = volumeCtr.value / 100;
+
+  const canvas = document.getElementById('visualizer');
+  const ctx = canvas.getContext('2d');
 
   volumeCtr.addEventListener('input', (ev) => {
     audioElement.volume = ev.target.value / 100;
   });
 
-  audioElement.volume = volumeCtr.value / 100;
-  await audioElement.play();
-
-  const audioSource = audioContext.createMediaElementSource(audioElement);
-  audioSource.connect(analyser);
-  analyser.connect(audioContext.destination);
-
-  const barWidth = canvas.width / bufferLength;
-  ctx.fillStyle = 'rgb(0, 0, 0)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Visualizer animation function
   function draw() {
     analyser.getByteFrequencyData(dataArray);
-    ctx.fillStyle = 'rgb(0, 0, 0)';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for (let i = 0; i < bufferLength; i++) {
+    canvas.width = canvas.parentElement.clientWidth; // Set canvas width to match its container
+    canvas.height = 150; // Set canvas height
+
+    const zeros = dataArray.filter((v) => v === 0);
+    const barWidth = canvas.width / (dataArray.length - zeros.length);
+
+    for (let i = 0; i < dataArray.length; i++) {
       const barHeight = dataArray[i];
-      const r = barHeight + 25 * (i / bufferLength);
-      const g = 250 * (i / bufferLength);
+      const r = barHeight + 25 * (i / dataArray.length);
+      const g = 250 * (i / dataArray.length);
       const b = 50;
-      ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+      ctx.fillStyle = `rgb(${r},${g},${b})`;
       ctx.fillRect(
         i * barWidth,
         canvas.height - barHeight,
@@ -52,12 +144,48 @@ const initializeAudio = async () => {
   }
 
   draw();
-};
+}
 
-// Play button click event
-const playButton = document.getElementById('playButton');
-
-playButton.addEventListener('click', async () => {
-  await initializeAudio();
-  playButton.disabled = true;
+const audioInput = document.getElementById('audioInput');
+audioInput.addEventListener('change', (event) => {
+  const selectedFile = event.target.files[0];
+  if (selectedFile) {
+    selectedAudio = selectedFile;
+    updateSelectedAudioName();
+    playButton.disabled = false;
+  }
 });
+
+const playButton = document.getElementById('playButton');
+playButton.addEventListener('click', async () => {
+  if (selectedAudio) {
+    const audioSrc = URL.createObjectURL(selectedAudio);
+    await initializeAudio(audioSrc);
+    playButton.disabled = true;
+    stopButton.disabled = false;
+  }
+});
+
+const stopButton = document.getElementById('stopButton');
+stopButton.addEventListener('click', () => {
+  if (audioElement) {
+    audioElement.pause();
+    audioElement.currentTime = 0;
+    stopButton.disabled = true;
+    playButton.disabled = false;
+  }
+});
+
+const playDefaultButton = document.getElementById('playDefaultButton');
+playDefaultButton.addEventListener('click', () => {
+  initializeAudio('tala-al.mp3');
+  playButton.disabled = true;
+  stopButton.disabled = false;
+});
+
+function updateSelectedAudioName() {
+  const selectedAudioNameElement = document.getElementById(
+    'selected-audio-name'
+  );
+  selectedAudioNameElement.textContent = selectedAudio.name;
+}
